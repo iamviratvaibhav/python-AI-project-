8b9a62aab4ff40be18336f58d9be37ca9e80f3e9
Vaibhav Verma <134628720+iamviratvaibhav@users.noreply.github.com>
2024-12-09 22:10:38 +0530

    This is chatBot with using python with AI library
    run this code in your pc with your groq api 
    enter the prompt - write 5 mcq in java for interview question with answer

---
+++ mcq.py
@@ -0,0 +1,20 @@
+
+
+from groq import Groq
+
+# Replace 'your_api_key_here' with your actual API key
+client = Groq(api_key="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")
+
+prompt = "Generate 5 multiple-choice question about the solar system with four options and indicate the correct answer."
+completion = client.chat.completions.create(
+    model="llama3-8b-8192",
+    messages=[{"role": "user", "content": prompt}],
+    temperature=1,
+    max_tokens=1024,
+    top_p=1,
+    stream=True,
+    stop=None,
+)
+
+for chunk in completion:
+    print(chunk.choices[0].delta.content or "", end="")
